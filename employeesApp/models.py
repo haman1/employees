0ecfd65eedf5b69830be5912cae40203d55ae180
ombasa <ombasaherman@gmail.com>
2021-09-05 22:59:21 +0300

    models

--- employeesApp/models.py
+++ employeesApp/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
 # Create your models here.
+
+class Department(models.Model):
+	DepartmentId = models.AutoField(primary_key=True)
+	DepartmentName = models.CharField(max_length=500)
+	
+	
+class Employees(models.Model):
+	EmployeeId = models.AutoField(primary_key=True)
+	EmployeeName = models.CharField(max_length=50)
+	Department = models.CharField(max_length=100)
+	DateOfJoining = models.DateField()
+	ProfileName = models.CharField(max_length=100)
